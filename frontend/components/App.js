f9b030da626b72fb073f76c2311e296e83516f51
DBehrashi <dari.behrashi@gmail.com>
2022-04-11 11:54:53 -0700

    Finished MVP

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import Form from './Form';
+import TodoList from './TodoList';
 
+const initialState = {
+  toDos: []
+};
 export default class App extends React.Component {
+  
+  state = initialState;
+
+  clearFinished = () => {
+    const filtered = this.state.toDos.filter(item => {
+      return !item.completed;
+    })
+    
+    this.setState({...this.state, toDos: filtered})
+  }
+  
+  addTodo = (task) => {
+    const newTask = {
+      id: Date.now(),
+      task: task,
+      completed: false
+    }
+
+    this.setState({...this.state, toDos: [...this.state.toDos, newTask]})
+  }
+
+  handleToggle = (task) => {
+    const filtered = this.state.toDos.map(item => {
+      if(item.id === task.id){
+        return {...item, completed: !item.completed};
+      }else{
+        return item;
+      }
+    })
+    this.setState({...this.state, toDos: filtered})
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList toDos={this.state.toDos} handleToggle={this.handleToggle}/>
+        <Form addTodo={this.addTodo} clearFinished={this.clearFinished}/>
       </div>
     )
   }
